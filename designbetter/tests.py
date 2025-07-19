4bb67fe39d04a20aed67ca4c6b10fe01fb1e0598
Luisfer2211 <luisferpalacioslopez@gmail.com>
2025-07-18 21:54:55 -0600

    Implementacion del Test correspondiente al Login

--- designbetter/tests.py
+++ designbetter/tests.py
@@ -18,3 +18,16 @@ class UserTestCase(TestCase):
     def test_user_creation(self):
         user = Usuario.objects.create_user(correo_electronico="testuser", nombre="usuario", password="12345")
         self.assertIsNotNone(user.id)
+
+class LoginTestCase(TestCase):
+
+    def setUp(self):
+        Usuario.objects.create_superuser(correo_electronico="testuser", nombre="usuario", password="12345")
+        
+    def test_login_success(self):
+        response = self.client.post("/auth/login/", {"correo_electronico": "testuser", "password": "12345"})
+        self.assertEqual(response.status_code, 200)  
+
+    def test_login_failure(self):
+        response = self.client.post("/auth/login/", {"correo_electronico": "wrong", "password": "wrong"})
+        self.assertEqual(response.status_code, 401)
