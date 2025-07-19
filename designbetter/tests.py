058065c78952a54ff80344429b67187dea3443ec
André Pivaral <andrepivaral@outlook.com>
2025-07-18 21:37:30 -0600

    Implementacion del Test correspondiente a la creacion de un usuario

--- designbetter/tests.py
+++ designbetter/tests.py
@@ -13,3 +13,8 @@ class AccountActivationTestCase(TestCase):
         response = self.client.get(f"/auth/activate/token_falso28ch8j8i32/")
         self.assertEqual(response.status_code, 400)
 
+
+class UserTestCase(TestCase):
+    def test_user_creation(self):
+        user = Usuario.objects.create_user(correo_electronico="testuser", nombre="usuario", password="12345")
+        self.assertIsNotNone(user.id)
