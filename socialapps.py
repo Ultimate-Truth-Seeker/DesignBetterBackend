09c638c81366da74200fbccbb59916f2bd7aef10
Ultimate-Truth-Seeker <96703353+Ultimate-Truth-Seeker@users.noreply.github.com>
2025-08-08 19:13:10 -0600

    feat: added sample objects for testing apis in frontend

--- socialapps.py
+++ socialapps.py
@@ -1,5 +1,7 @@
 from allauth.socialaccount.models import SocialApp
 from django.contrib.sites.models import Site
+from ecommerce.models import EstadoPedido
+from designbetter.models import PlantillaPrenda
 
 site = Site.objects.get(id=1)
 
@@ -10,3 +12,14 @@ app = SocialApp.objects.create(
     secret='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'
 )
 app.sites.add(site)
+
+EstadoPedido.objects.create(
+    slug='pendiente', nombre='Pendiente', orden=1
+)
+EstadoPedido.objects.create(
+    slug='diseno', nombre='En Diseño', orden=2
+)
+EstadoPedido.objects.create(
+    slug='produccion', nombre='Producción', orden=3
+)
+PlantillaPrenda.objects.create(nombre = "plantilla ejemplo", descripcion = "", tipo_ropa = "", tipo_cuerpo = "")
